450f740672a9bdd5f742be7770e47f3f325ed13a
techstacksavvy <saheeny@yahoo.com>
2022-08-16 19:13:18 -0500

    Initial Script

---
+++ sandwich.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+echo Welcome to Wich Sandwich!
+echo " "
+echo "Here are our Menu Options:"
+echo " "
+echo Melty Double Red Pepper Panini(veggie) 
+echo Meatball(meaty)
+echo Club(cold cut)
+echo " "
+echo "Which sandwich would you like to make? " ans
+
+
